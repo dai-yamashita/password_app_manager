0531e5d5ac42daa148b18212e3975849753229c8
Arman Ortega <ortegaaa@ph.ibm.com>
2016-03-10 18:50:05 +0800

    Added site settings

---
+++ system/application/libraries/sitesettings.php
@@ -0,0 +1,26 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Sitesettings {
+
+    var $CI;
+
+    function __construct() {
+        $this->CI =& get_instance();
+        $this->CI->config->load('sitesettings');
+        $this->CI->load->library('logger');
+        define('BASEURL', $this->CI->config->item('base_url') );
+        define('UPLOADDIR', './uploads/' );
+        define('THEMEPATH_IMG', BASEURL.'themes/default/images/');
+        $this->CI->logger->removetemp( array('tempfolder' => UPLOADDIR . 'csv/' ));
+    }
+
+    function get_settings($k) {
+        $this->CI->db->where('key', $k);
+        $tmp = $this->CI->db->get('sitesettings')->row_array();
+        $tmp = $tmp['value'];
+        return $tmp;
+    }
+
+}	
+
+
