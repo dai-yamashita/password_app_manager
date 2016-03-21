3b87069ebad1b38e50f12d7e5fd8c32f3b2c3521
Arman Ortega <ortegaaa@ph.ibm.com>
2016-03-21 19:35:07 +0800

    Added views footer

---
+++ system/application/views/default/footer.php
@@ -0,0 +1,19 @@
+
+</script>
+<?php 
+$timezone = $this->sitesettings->get_settings('timezone');
+$isdaylightsaving = $this->sitesettings->get_settings('isdaylightsaving');
+$localtime = gmt_to_local(now(), $timezone, $isdaylightsaving );
+$localtime = date('F d Y g:i:s a', $localtime);
+?>
+	<div id="footer">
+<!--		<div id="menu" style="display:none">
+			<a href="#" title="Home">Home</a>
+			<a href="#" title="Administration">Administration</a>
+			<a href="#" title="Settings">Settings</a>
+			<a href="#" title="Contact">Contact</a>
+		</div>-->
+        <div id="menu" >
+        Local time: <?php echo $localtime  ?>
+        </div>
+		Copyright &copy; 2009 - Trimorp</div>
