81bfe842b5fd34262d581d707cc5d9e9cc12b44d
Arman Ortega <ortegaaa@ph.ibm.com>
2016-04-15 16:26:48 +0800

    Added projects import

---
+++ system/application/views/default/projects_import.php
@@ -0,0 +1,44 @@
+<div class="title title-spacing">
+    <h2>Import projects</h2>
+    <span style="color:#000; font-size:14px">Import groups in <b><?= $importfmt?></b> format.</span><br /><br />
+	<?php if ($importfmt == 'csv') { ?>    
+	<a href="<?= base_url() ?>uploads/sample/sample-project.csv" >Click here to Download sample <?= $importfmt?> format &raquo;.</a>
+    <?php } elseif ($importfmt == 'xml') { ?>
+	<a href="<?= base_url() ?>uploads/sample/sample-project.xml" >Click here to Download sample <?= $importfmt?> format &raquo;.</a>    
+    <?php } ?>
+            
+</div>
+<?php !empty($flash) ? flash($flash) : '' ?>
+
+<form method="post" action="<?= site_url("admin/project/import/$importfmt") ?>"  id="exportfrm"  enctype="multipart/form-data" >
+<?php if ( $importfmt == 'csv') { ?>
+<input type="file" name="userfile" class="field text large"   />
+ <table width="99%" border="1" class="csvoptable" >
+  <tr>
+    <td colspan="2"><p><b>CSV options</b><br />Please check your csv file and determine the enclosure character and field delimiter character.</p></td>
+    </tr>
+  <tr>
+    <td width="92">Enclosed by</td>
+    <td width="826"><input type="text" name="enclosure" size="4" value='"' /></td>
+  </tr>
+  <tr>
+    <td>Delimiter</td>
+    <td><input type="text" name="delimiter" size="4" value="," /></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="btnimport" value="Import"  /></td>
+  </tr>
+</table>
+<?php } else { ?>
+<input type="file" name="userfile" class="field text large"   />
+<p>Please browse your xml file and click Import button.</p>
+&nbsp;&nbsp;<input type="submit" name="btnimport" value="Import" />
+<?php } ?>
+
+<input type="hidden" name="importfmt" value="<?= $importfmt ?>"  />
+</form>
