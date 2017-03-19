d0dc0c4662e0d04c92b638221150cb39cb23deb0
Arman Ortega <sudogem@gmail.com>
2017-03-20 01:35:11 +0800

    Added migrations: department,user groups, domains

---
+++ db/migrations/20170319173135_domains.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Domains extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+      // create the table
+      $table = $this->table('domains');
+      $table->addColumn('project_id', 'integer')
+            ->addColumn('type', 'string')
+            ->addColumn('templateid', 'integer')
+            ->addColumn('importance', 'string')
+            ->addColumn('url', 'string')
+            ->addColumn('loginurl', 'integer')
+            ->addColumn('username', 'string')
+            ->addColumn('password', 'string')
+            ->addColumn('pwlength', 'string')
+            ->addColumn('mark', 'string')
+            ->addColumn('notes', 'string')
+            ->create();
+    }
+}
