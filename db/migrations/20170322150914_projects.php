e8cdf4c87ab6975746e6e6fde6140c663986f134
Arman Ortega <ortegaaa@ph.ibm.com>
2017-03-22 23:19:14 +0800

    Add migration file: projects

---
+++ db/migrations/20170322150914_projects.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Projects extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+      $table = $this->table('projects');
+      $table->addColumn('projectid', 'integer')
+            ->addColumn('domain_id', 'integer')
+            ->addColumn('user_id', 'integer')
+            ->addColumn('project', 'string')
+            ->addColumn('desc', 'string')
+            ->addColumn('visibility', 'string')
+            ->addColumn('created', 'datetime')
+            ->create();
+    }
+}
