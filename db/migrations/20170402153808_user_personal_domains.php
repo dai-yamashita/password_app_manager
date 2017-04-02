29f8d182beab9055c97ff519e3ec0c61c6487a5a
Arman Ortega <sudogem@gmail.com>
2017-04-02 23:59:22 +0800

    Added migration: user_personal_domains

---
+++ db/migrations/20170402153808_user_personal_domains.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class UserPersonalDomains extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+      $table = $this->table('user_personal_domains');
+      $table->addColumn('user_id', 'integer')
+            ->addColumn('domain_id', 'integer')
+            ->create();
+    }
+}
