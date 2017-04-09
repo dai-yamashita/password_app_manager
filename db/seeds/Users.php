f5b0ca4896d416b192ae218c241cda0e970019c3
Arman Ortega <sudogem@gmail.com>
2017-04-10 03:35:45 +0800

    Update seeds/Users

--- db/seeds/Users.php
+++ db/seeds/Users.php
@@ -15,16 +15,35 @@ class Users extends AbstractSeed
     public function run()
     {
       $data = array(
-        'tmpid' => 1,
-        'username' => 'admin',
-        'password' => '74be16979710d4c4e7c6647856088456',
-        'firstname' => 'admin',
-        'lastname' => 'admin',
-        'email' => 'admin@mail.com',
-        'skypeid' => 'admin',
-        'role_id' => 2,
-        'position' => '',
+        array(
+          'tmpid' => 1,
+          'username' => 'admin',
+          'password' => '2751819b092e7d976bb47f8c56e0ec64',
+          'firstname' => 'admin',
+          'lastname' => 'admin',
+          'email' => 'admin@mail.com',
+          'role_id' => 1,
+        ),
+        array(
+          'tmpid' => 2,
+          'username' => 'manager',
+          'password' => '0fe49e03d86e6981be3fe4551075f3ba',
+          'firstname' => 'manager',
+          'lastname' => 'manager',
+          'email' => 'manager@mail.com',
+          'role_id' => 2,
+        ),
+        array(
+          'tmpid' => 2,
+          'username' => 'user1',
+          'password' => '038e2e4e4ba903e1c2a9a6824ebf104d',
+          'firstname' => 'user1',
+          'lastname' => 'user1',
+          'email' => 'user1@mail.com',
+          'role_id' => 3,
+        )
       );
+
       $table = $this->table('users');
       $table->insert($data)
             ->save();
